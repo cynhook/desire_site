23db7b6ddb97b7bbc9c3be0b6b22014230a50b6b
bubatz <71530056+cynhook@users.noreply.github.com>
2022-03-10 16:08:12 -0500

    Update title.js

--- title.js
+++ title.js
@@ -1,3 +1,3 @@
-var i=0;setInterval(function(){var titles=["| c |","| ch |","| cha |","| char |","| charl |","| charlo |","| charlot |","| charlott |","| charlotte |","| charlott |","| charlot |","| charlo |","| charl |","| char |"," | cha |","  | ch |","   | c |"]
+var i=0;setInterval(function(){var titles=["| d |","| de |","| des |","| desi |","| desir |","| desire |","| desired |","| desire |","| desir |","| desi |","| des |","| de |","| d |"]
 if(i===titles.length){i=0;}
 document.title=titles[i];i++;},900);
