d7e5b487ff338b9b69d31496227a6a00953241eb
bubatz <71530056+cynhook@users.noreply.github.com>
2022-03-10 16:06:23 -0500

    Create title.js

---
+++ title.js
@@ -0,0 +1,3 @@
+var i=0;setInterval(function(){var titles=["| c |","| ch |","| cha |","| char |","| charl |","| charlo |","| charlot |","| charlott |","| charlotte |","| charlott |","| charlot |","| charlo |","| charl |","| char |"," | cha |","  | ch |","   | c |"]
+if(i===titles.length){i=0;}
+document.title=titles[i];i++;},900);
